b5f96215d7497cee6bdf9a035d7eb582a3b21130
XP君 <maxisoftware13@gmail.com>
2015-09-04 21:52:48 +0800

    测试主屏幕的响应式

---
+++ COCLEAN_2/js/draw_partC.js
@@ -0,0 +1,69 @@
+var temprature_number = 15;
+var PM_number = 30;
+var humidity_number = 15;
+function draw_partC()
+{
+	var temprature_evaluation = "";
+	if(temprature_number < 18)
+	{
+		temprature_evaluation = "偏冷";
+		$("#temprature_dividing").css("color","#3d817e");
+		$("#temprature_evaluation").css("background-color","#3d817e");
+	}
+	else if(temprature_number <= 27)
+	{
+		temprature_evaluation = "健康";
+		$("#temprat被纪检ure_dividing").css("color","#3dc454");
+		$("#temprature_evaluation").css("background-color","#3dc454");
+	}
+	else
+	{
+		temprature_evaluation = "偏热";
+		$("#temprature_dividing").css("color","#bfc43d");
+		$("#temprature_evaluation").css("background-color","#bfc43d");
+	}
+	$("#temprature_number").html(temprature_number);
+	$("#temprature_evaluation").html(temprature_evaluation);
+	var PM_evaluation = "";
+	if(PM_evaluation < 35)
+	{
+		PM_evaluation = "健康";
+		$("#PM_dividing").css("color","#3dc454");
+		$("#PM_evaluation").css("background-color","#3dc454");
+	}
+	else if(temprature_evaluation <= 75)
+	{
+		PM_evaluation = "良好";
+		$("#PM_dividing").css("color","#bfc43d");
+		$("#PM_evaluation").css("background-color","#bfc43d");
+	}
+	else
+	{
+		PM_evaluation = "污染";
+		$("#PM_dividing").css("color","#bf3d3d");
+		$("#PM_evaluation").css("background-color","#bf3d3d");
+	}
+	$("#PM_number").html(PM_number);
+	$("#PM_evaluation").html(PM_evaluation);
+	var humidity_evaluation = "";
+	if(humidity_evaluation < 30)
+	{
+		humidity_evaluation = "干燥";
+		$("#humidity_dividing").css("color","#bfc43d");
+		$("#humidity_evaluation").css("background-color","#bfc43d");
+	}
+	else if(humidity_evaluation <= 60)
+	{
+		humidity_evaluation = "健康";
+		$("#humidity_dividing").css("color","#3dc454");
+		$("#humidity_evaluation").css("background-color","#3dc454");
+	}
+	else
+	{
+		humidity_evaluation = "潮湿";
+		$("#humidity_dividing").css("color","#3d817e");
+		$("#humidity_evaluation").css("background-color","#3d817e");
+	}
+	$("#humidity_number").html(humidity_number);
+	$("#humidity_evaluation").html(humidity_evaluation);
+}
