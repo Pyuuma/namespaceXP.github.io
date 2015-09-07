250221017ead4e1fd78bf2616e118c0e7d468932
XP君 <maxisoftware13@gmail.com>
2015-09-07 13:14:45 +0800

    no message

---
+++ COCLEAN_2副本 3/js/draw_partB.js
@@ -0,0 +1,76 @@
+function draw_partB()
+{
+	var sum = 0;
+	var new_data = new Array;
+	for(var i = 0; i < 48; i++)
+	{
+		new_data[i] = 0;
+	}
+	var useful_data_number = 0;
+	for(var i = 0; i < 96; i ++)
+	{
+		sum += data[i];
+		if (data[i] != 0)
+		{
+			useful_data_number++;
+		}
+		new_data[Math.floor(i/2)] += data[i];
+	}
+	//取15分钟数据点的平均值
+	for(var i = 0; i < 48; i++)
+	{
+		new_data[i] = parseInt(new_data[i]/2);
+	}
+	average = parseInt(sum/useful_data_number);
+	$("#chart_average").html("今日均值:"+average);
+	var date = new Date();
+	this.hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+	this.minute = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+	var current_time = this.hour +　":" + this.minute;
+	$("#chart_time").html("更新时间:"+current_time);
+	var max = Math.max.apply(null, new_data);
+	var min = max;
+	for(var i = 0; i < 48; i++)
+	{
+		if(new_data[i] < min && new_data[i] != 0)
+		{
+			min = new_data[i];
+		}
+	}
+	var step = Math.ceil(max * 1.1 / 30) *30 / 6;
+	debugger;
+	for(var i = 1; i <= 6; i ++)
+	{		
+		$("#amount_line"+i).html(step * i);
+	}
+	var pillar_position = ($("#chart").width() - $("#amount_line").width())/48;
+	var pillar_width = ($("#chart").width() - $("#amount_line").width())/52;
+	var pillar_height = $("#chart").height()/(step*6);
+	var min_height = $("#chart").height() * 0.1;
+	for(var i = 0; i < 48; i++)
+	{
+		$("#pillar"+i).css("left",i * pillar_position);
+		$("#pillar"+i).css("width", pillar_width);
+		if(new_data[i]!=0)
+		{
+			$("#pillar"+i).css("height", (new_data[i]) * pillar_height);
+		}
+		if(new_data[i]<35)
+		{
+			$("#pillar"+i).css("background-color","#3dc454");
+		}
+		else if(new_data[i] <= 75)
+		{
+			$("#pillar"+i).css("background-color","#bfc43d");
+		}
+		else
+		{
+			$("#pillar"+i).css("background-color","#c43d3d");
+		}
+	}
+	for(var i = 1; i <= 4; i++)
+	{
+		$("#time_line"+i).css("left", (12*i - 2)*pillar_position);
+		$("#time_line"+i).css("width", 2*pillar_position);
+	}
+}
