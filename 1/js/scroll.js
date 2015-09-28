dee89df86204e1f2dbf8e92b157bb58135ead1a8
XP君 <maxisoftware13@gmail.com>
2015-09-29 01:29:53 +0800

    no message

--- 1/js/scroll.js
+++ 1/js/scroll.js
@@ -2,9 +2,9 @@ var iframeLoaded = function (iframe) {
     if (iframe.src.length > 0) {
         if (!iframe.readyState || iframe.readyState == "complete") {
             var bHeight = 
-            iframe.contentWindow.document.body.scrollHeight;
+            iframe.contentWindow.document.body.innerHeight;
             var dHeight = 
-            iframe.contentWindow.document.documentElement.scrollHeight;
+            iframe.contentWindow.document.documentElement.innerHeight;
             var height = Math.max(bHeight, dHeight);
             iframe.height = height;
         }
