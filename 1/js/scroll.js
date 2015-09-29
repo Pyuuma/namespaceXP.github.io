c49190b4b3e5593b7001a3f268b226e2eca652c9
XP君 <maxisoftware13@gmail.com>
2015-09-29 10:58:31 +0800

    no message

--- 1/js/scroll.js
+++ 1/js/scroll.js
@@ -1,12 +1,40 @@
-var iframeLoaded = function (iframe) {
-    if (iframe.src.length > 0) {
-        if (!iframe.readyState || iframe.readyState == "complete") {
-            var bHeight = 
-            iframe.contentWindow.document.body.innerHeight;
-            var dHeight = 
-            iframe.contentWindow.document.documentElement.innerHeight;
-            var height = Math.max(bHeight, dHeight);
-            iframe.height = height;
-        }
+window.onscroll = function () {
+	{
+		if(getScrollTop() > $('#page2').offset().top - 10 && getScrollTop() < $('#page2').offset().top + 10){
+			document.getElementById('page2').contentWindow.anima();
+		}
+	}
+}
+
+
+
+
+
+function getScrollTop(){
+    var scrollTop = 0;
+    if(document.documentElement&&document.documentElement.scrollTop){
+        scrollTop=document.documentElement.scrollTop;
     }
+    else if(document.body){
+        scrollTop=document.body.scrollTop;
+    }
+    return scrollTop;
+}
+
+
+
+function getClientHeight(){
+    var clientHeight=0;
+    if(document.body.clientHeight&&document.documentElement.clientHeight){
+        var clientHeight = (document.body.clientHeight<document.documentElement.clientHeight)?document.body.clientHeight:document.documentElement.clientHeight;        
+    }
+    else{
+        var clientHeight = (document.body.clientHeight>document.documentElement.clientHeight)?document.body.clientHeight:document.documentElement.clientHeight;    
+    }
+    return clientHeight;
+}
+
+
+function getScrollHeight(){
+    return Math.max(document.body.scrollHeight,document.documentElement.scrollHeight);
 }
