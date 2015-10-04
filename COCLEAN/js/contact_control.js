fd2a0b5313485a06b5777ef9e27297a613b5090a
XP君 <maxisoftware13@gmail.com>
2015-10-04 21:23:10 +0800

    no message

--- COCLEAN/js/contact_control.js
+++ COCLEAN/js/contact_control.js
@@ -4,11 +4,12 @@ var height = (document.all ? document.getElementsByTagName("html")[0].offsetHeig
 var top_padding = 8 * width;
 var left_padding = 4 * width;
 var content_fontsize = 2.8 * width;
-var job_fontrate = 3.2; 
+var job_fontrate = 3.6; 
 var contact_height = 32 * width;
 var lineheight_rate =1.8; 
 var language_height = 9.6 * width;
 var copyright_height = 2 * width;
+var chosen = 1;
 
 window.onload = function(){
 	$('#header').css('height', $('#header_image').height().toString() + 'px');
@@ -58,16 +59,16 @@ window.onload = function(){
 	$('#copyright').css('bottom', ((parseInt($('#curve').css('bottom')) - $('#copyright').height()) / 2).toString() + 'px');
 	
 	$('#language_img').css('height', (parseInt($('#curve').css('bottom')) * 0.7).toString() + 'px');
-	$('#language_img').css('right', (19 * width).toString() + 'px');
+	$('#language_img').css('right', (22 * width).toString() + 'px');
 	$('#language_img').css('bottom', ((parseInt($('#curve').css('bottom')) - $('#language_img').height()) / 2).toString() + 'px');
 	
 	$('#change_img').css('height', (parseInt($('#curve').css('bottom')) * 0.16).toString() + 'px');
-	$('#change_img').css('right', (4.6 * width).toString() + 'px');
+	$('#change_img').css('right', (4.5 * width).toString() + 'px');
 	$('#change_img').css('bottom', ((parseInt($('#curve').css('bottom')) - $('#change_img').height()) / 2).toString() + 'px');
 	
 	$('#language_now').css('height', (parseInt($('#curve').css('bottom')) * 0.29).toString() + 'px');
 	$('#language_now').css('font-size', $('#language_now').height().toString() + 'px');
-	$('#language_now').css('right', (10 * width).toString() + 'px');
+	$('#language_now').css('right', (12 * width).toString() + 'px');
 	$('#language_now').css('bottom', ((parseInt($('#curve').css('bottom')) - $('#language_now').height()) / 2).toString() + 'px');
 	
 	$('#content').css('top', ($('#job_list').height() + $('#header').height()).toString() + 'px');
@@ -104,6 +105,21 @@ job2.onclick = function(){
 job3.onclick = function(){
 	$('#duty').html('-&nbsp;网站内共建设的布局和结构等方面的整体规划和文字编辑工作<br>-&nbsp;负责网站日常美术设计和宣传资料的制作，编写网站宣传资料及相关<br>&nbsp;&nbsp;产品资料<br>-&nbsp;配合策划推广活动，并参与执行<br>');
 	$('#requirement').html('-&nbsp;美术相关专业，具有深厚的美术功底和良好的创意构思能力、综合视<br>&nbsp;&nbsp;觉把握能力<br>-&nbsp;熟练使用Photoshop、AI等常用设计制作软件<br>-&nbsp;工作认真，有责任心，踏实肯干，有团队合作精神<br>-&nbsp;具有一定的美术功底，出色的创意构思能力<br>');
+}
+
+job4.onclick = function(){
+	$('#duty').html('-&nbsp;负责产品的构思、设计、造型等<br>-&nbsp;将品牌、产品规划以及产品定位物化为合适的产品设计<br>-&nbsp;将好的产品设计转化为好的商品和用品<br>');
+	$('#requirement').html('-&nbsp;1年以上实战经验，产品设计、工业设计等相关专业但并不局限，重点<br>&nbsp;&nbsp;是出色的造型表达能力<br>-&nbsp;对材料、工艺、表面处理有丰富的经验和独特的视角，并结合设计趋势<br>&nbsp;&nbsp;进行突破和创新<br>-&nbsp;同相关部门一起解决产品开发过程中的问题<br>-&nbsp;良好的沟通能力，富有团队合作精神<br>');
+}
+
+job5.onclick = function(){
+	$('#duty').html('-&nbsp;配合品牌及产品推广、营销策划撰写相关稿件，包括新闻稿、深度稿<br>&nbsp;&nbsp;与评论稿等<br>-&nbsp;对品牌及产品进行精准如实的描述，负责产品及品牌相关的广告创意<br>&nbsp;&nbsp;文案的撰写<br>-&nbsp;负责行业相关资讯的采集，微信、微博、论坛等相关平台的内容编辑<br>&nbsp;&nbsp;发布（综合文字、图片等资料），能够独立策划内容主题<br>-&nbsp;协助产品对外宣传资料的编辑撰写和包装等<br>');
+	$('#requirement').html('-&nbsp;本科以上学历，中文、广告、传播、新闻、营销等相关专业，1年以上<br>&nbsp;&nbsp;文案实战经验，优秀的文案撰写、策划能力及执行力，思维活跃，思路<br>&nbsp;&nbsp;开阔，逻辑性强，文笔好，文案撰写效率高<br>-&nbsp;对微博、微信、论坛等有深入了解并有独到见解，热衷活跃于各大交流<br>&nbsp;&nbsp;平台<br>-&nbsp;有激情和动力，具有较强的责任心和团队合作精神，熟练使用Word、<br>&nbsp;&nbsp;Excel、PowerPoint等办公软件<br>');
+}
+
+job6.onclick = function(){
+	$('#duty').html('-&nbsp;嵌入式系统开发，熟悉ARM和STM32内核<br>-&nbsp;外设驱动开发<br>-&nbsp;现配合系统联调测试<br>');
+	$('#requirement').html('-&nbsp;通信或计算机专业本科及以上学历<br>-&nbsp;有实际项目开发经验，熟知相关系统运行机制<br>-&nbsp;有蓝牙或WiFi相关项目开发经验者优先<br>');
 }
 
 
