aa2db0132cdf236abb2ba0b9bc48c6c711ddc04b
XP君 <maxisoftware13@gmail.com>
2015-10-10 10:36:26 +0800

    no message

--- COCLEAN/js/set_header_footer.js
+++ COCLEAN/js/set_header_footer.js
@@ -14,21 +14,18 @@ var goto_addus = document.getElementById('goto_addus');
 var header_button = document.getElementById('header_button');
 
 
-var top_padding = 8 * width;
-var left_padding = 4 * width;
 var content_fontsize = 2.8 * width;
-var header_fontsize = 3.6 * width;
-var job_fontrate = 3.6; 
-var contact_height = 32 * width;
+var header_fontsize = 3.6;
+var contact_height = 32;
 var lineheight_rate = 1.8; 
 var goto_lineheight_rate = 3.6; 
-var language_height = 9.6 * width;
-var copyright_height = 2 * width;
+var language_height = 9.6;
+var copyright_height = 2;
 var chosen = 1;
 var language = 1;
-var language_now_width = 13 * width;
-var change_language_innerwidth = 2 * width;
-var change_language_height = 16 * width;
+var language_now_width = 13;
+var change_language_innerwidth = 2;
+var change_language_height = 16;
 var header_full_height = 86;
 var wechat_load_width = 15;
 var goto_button_height = 13.2;
@@ -39,8 +36,8 @@ function set_header(){
 	$('#header_button').css('height', $('#header_image').height().toString() + 'px');
 	$('#goto').css('top', $('#header_image').height().toString() + 'px');
 	$('#goto').css('height', (header_full_height  * width - $('#header_image').height()).toString() + 'px');
-	$('#goto').css('font-size', header_fontsize.toString() + 'px');
-	$('#goto').css('line-height', (header_fontsize * goto_lineheight_rate).toString() + 'px');
+	$('#goto').css('font-size', (header_fontsize * width).toString() + 'px');
+	$('#goto').css('line-height', (header_fontsize * goto_lineheight_rate * width).toString() + 'px');
 	$('.goto_button').css('height', (goto_button_height * width).toString() + 'px');
 	$('.goto_button_word').css('height', (goto_button_height * width).toString() + 'px');
 	$('.goto_button_word').css('line-height', (goto_button_height * width).toString() + 'px');
@@ -57,10 +54,10 @@ function set_change_language(){
 		top_now = 100 * height -  contact_height;
 	}
 	$('#contact').css('top', top_now.toString() + 'px');
-	$('#contact').css('height', contact_height.toString() + 'px');
+	$('#contact').css('height', (contact_height * width).toString() + 'px');
 	$('#contact_title').css('font-size',(3.8 * width).toString() + 'px');
 	$('#contact_address').css('font-size',(2.8 * width).toString() + 'px');
-	$('#copyright').css('height',copyright_height.toString() + 'px');
+	$('#copyright').css('height',(copyright_height * width).toString() + 'px');
 	$('#copyright').css('font-size', $('#copyright').height().toString() + 'px');
 	$('#copyright').css('line-height', $('#copyright').height().toString() + 'px');
 	$('#contact_address').css('line-height',(1.8 * parseInt($('#contact_address').css('font-size'))).toString() + 'px');
@@ -75,15 +72,15 @@ function set_change_language(){
 	$('#wechat_load').css('right',(parseInt($('#wechat_img').css('right')) + $('#wechat_img').width() / 2).toString() + 'px');
 	$('#wechat_load').css('width',(wechat_load_width * width).toString() + 'px');
 	$('#wechat_load').css('top',(parseInt($('#wechat_img').css('top')) - $('#wechat_load').height()).toString() + 'px');
-	$('#curve').css('bottom', language_height.toString() + 'px');
+	$('#curve').css('bottom', (language_height * width).toString() + 'px');
 	$('#copyright').css('bottom', ((parseInt($('#curve').css('bottom')) - $('#copyright').height()) / 2).toString() + 'px');
 	
 	$('#change_img').css('height', (parseInt($('#curve').css('bottom')) * 0.16).toString() + 'px');
 	$('#change_img').css('right', (4.5 * width).toString() + 'px');
-	$('#change_img').css('bottom', ((language_height - $('#change_img').height()) / 2).toString() + 'px');
+	$('#change_img').css('bottom', ((language_height * width - $('#change_img').height()) / 2).toString() + 'px');
 	
 	$('#language_now').css('height', (parseInt($('#curve').css('bottom')) * 0.29).toString() + 'px');
-	$('#language_now').css('width', language_now_width.toString() + 'px');
+	$('#language_now').css('width', (language_now_width * width).toString() + 'px');
 	$('#language_now').css('font-size', $('#language_now').height().toString() + 'px');
 	$('#language_now').css('line-height', $('#language_now').height().toString() + 'px');
 	$('#language_now').css('right', (parseInt($('#change_img').css('right')) + $('#change_img').width()).toString() + 'px');
@@ -95,31 +92,31 @@ function set_change_language(){
 	
 	$('#other_language_img').css('height', $('#language_img').height().toString() + 'px');
 	$('#this_language_img').css('height', $('#language_img').height().toString() + 'px');
-	$('#change_language').css('height', change_language_height.toString() + 'px');
-	$('#change_language').css('width', (2 * change_language_innerwidth + language_now_width + $('#change_img').height() + $('#language_img').height()).toString() + 'px');
+	$('#change_language').css('height', (change_language_height * width).toString() + 'px');
+	$('#change_language').css('width', (2 * change_language_innerwidth * width + language_now_width * width + $('#change_img').height() + $('#language_img').height()).toString() + 'px');
 	
 	$('#another_change_img').css('height', $('#change_img').height().toString() + 'px');
-	$('#another_change_img').css('right', change_language_innerwidth.toString() + 'px');
-	$('#another_change_img').css('bottom', (((change_language_height / 2) - $('#change_img').height()) / 2).toString() + 'px');
+	$('#another_change_img').css('right', (change_language_innerwidth * width).toString() + 'px');
+	$('#another_change_img').css('bottom', (((change_language_height  * width / 2) - $('#change_img').height()) / 2).toString() + 'px');
 	
 	$('.language').css('height', (parseInt($('#curve').css('bottom')) * 0.29).toString() + 'px');
-	$('.language').css('width', language_now_width.toString() + 'px');
+	$('.language').css('width', (language_now_width * width).toString() + 'px');
 	$('.language').css('font-size', $('#this_language').height().toString() + 'px');
 	$('.language').css('line-height', $('#this_language').height().toString() + 'px');
 	$('.language').css('right', (parseInt($('#another_change_img').css('right')) + $('#another_change_img').width()).toString() + 'px');
-	$('.language').css('bottom', (((change_language_height / 2) - $('#this_language').height()) / 2).toString() + 'px');
+	$('.language').css('bottom', (((change_language_height * width / 2) - $('#this_language').height()) / 2).toString() + 'px');
 	
 	$('#this_language_img').css('right', (parseInt($('.language').css('right')) + $('.language').width()).toString() + 'px');
-	$('#this_language_img').css('bottom', (((change_language_height / 2) - $('#language_img').height()) / 2).toString() + 'px');
+	$('#this_language_img').css('bottom', (((change_language_height * width / 2) - $('#language_img').height()) / 2).toString() + 'px');
 	$('#other_language_img').css('right', (parseInt($('.language').css('right')) + $('.language').width()).toString() + 'px');
-	$('#other_language_img').css('bottom', (((change_language_height / 2) - $('#language_img').height()) / 2).toString() + 'px');
+	$('#other_language_img').css('bottom', (((change_language_height * width / 2) - $('#language_img').height()) / 2).toString() + 'px');
 	
 	$('#change_language').css('right', (parseInt($('#change_img').css('right')) - parseInt($('#another_change_img').css('right')) - 1).toString() + 'px');
 	$('#change_language').css('bottom', (parseInt($('#change_img').css('bottom')) - parseInt($('#another_change_img').css('bottom')) - 1).toString() + 'px');
 	
 	$('#change_language').css('display', 'none');
 	
-	top_now += contact_height;
+	top_now += contact_height * width;
 }
 
 function set_language(){
