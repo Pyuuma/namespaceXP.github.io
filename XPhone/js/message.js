e8a9b06f25fefa5ade1ef2b53f0daa63a1d59211
namespaceXP <maxisoftware13@gmail.com>
2017-10-19 13:35:59 +0800

    Nothing serious
    yeah

--- XPhone/js/message.js
+++ XPhone/js/message.js
@@ -1,43 +1,14 @@
+var messagelist;
+
 $(document).ready(function(){
-	mytime();
-	setInterval(function() {mytime()},60000);
+	getMainMessageList();
 });
 
-function mytime() {
-    var date = new Date();
-    var seperator1 = "-";
-    var seperator2 = ":";
-   	var hour = date.getHours();
-	var min = date.getMinutes();
-	var mytime = document.getElementById("time");
-    if (hour >= 1 && hour <= 9) {
-        hour = "0" + hour;
-    }
-    if (min >= 0 && min <= 9) {
-        min = "0" + min;
-    }
-	
-    var currentdate = hour + seperator2 + min;
-	mytime.innerHTML = currentdate;
-    return currentdate;
+function getMainMessageList(){
+	$.getJSON("http://namespaceXP.github.io/XPhone/messages.json", function(json){
+		for(var i = 0; i < json.messages.length; i++){
+			
+		}
+	});
 }
 
-
-$("#phonebutton").click(function(){
-	$("#dial").css("display", "block");
-})
-
-$("#messagebutton").click(function(){
-	$("#message").css("display", "block");
-})
-
-$("#photobutton").click(function(){
-	$("#photo").css("display", "block");
-})
-
-
-$("#myhome").click(function(){
-	$("#dial").css("display", "none");
-	$("#message").css("display", "none");
-	$("#photo").css("display", "none");
-})
