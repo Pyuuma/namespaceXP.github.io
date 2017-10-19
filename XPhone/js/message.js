745b4fe642ed1883375ac9f518eb95e6e6f801cc
namespaceXP <maxisoftware13@gmail.com>
2017-10-19 16:09:06 +0800

    new

--- XPhone/js/message.js
+++ XPhone/js/message.js
@@ -1,4 +1,4 @@
-var messagelist;
+var messagelist = document.getElementById("messagelist");
 
 $(document).ready(function(){
 	getMainMessageList();
@@ -7,8 +7,39 @@ $(document).ready(function(){
 function getMainMessageList(){
 	$.getJSON("http://namespaceXP.github.io/XPhone/messages.json", function(json){
 		for(var i = 0; i < json.messages.length; i++){
-			
+			var newdiv = newMessagediv(json.messages[i].datetime, json.messages[i].number, json.messages[i].content);
+			messagelist.appendChild(newdiv);
 		}
 	});
+}
+
+function newMessagediv(date, number, content){
+	var maxlength = 22;
+	
+	var messagediv = document.createElement("div");
+	var datediv = document.createElement("div");
+	var numberdiv = document.createElement("div");
+	var contentdiv = document.createElement("div");
+	messagediv.id = number;
+	messagediv.className = "messagediv";
+	datediv.className = "datediv";
+	numberdiv.className = "numberdiv";
+	contentdiv.className = "contentdiv";
+	datediv.innerHTML = date;
+	numberdiv.innerHTML = number;
+	if(content.length <= maxlength){
+		contentdiv.innerHTML = content;
+	}
+	else{
+		contentdiv.innerHTML = content.substr(0, maxlength-2) + "……";
+	}
+	
+	messagediv.appendChild(datediv);
+	messagediv.appendChild(numberdiv);
+	messagediv.appendChild(contentdiv);
+	messagediv.onclick = function(){
+		
+	}
+	return messagediv;
 }
 
