a2c8d34bebda62609809f644fcfae09dc4e1bbdc
namespaceXP <maxisoftware13@gmail.com>
2017-11-28 21:57:02 +0800

    new

---
+++ test/js/button.js
@@ -0,0 +1,6 @@
+$("#submit").click(function(){
+	$("#quizpage").css("z-index","1");
+	$("#quizpage").css("opacity","0");
+	$("#nextpage").css("opacity","1");
+	$("#nextpage").css("z-index","2");
+})
